bcda753dfb5b9eaba0caf016021a9cb3ce8bc536
safestak-keith <keith@safestak.com>
2021-07-21 16:24:31 +0200

    Packing as a .NET cross-platform global tool

--- Src/ConsoleTool/Program.cs
+++ Src/ConsoleTool/Program.cs
@@ -11,17 +11,16 @@ namespace Cscli.ConsoleTool
             var cts = SetupUserInputCancellationTokenSource();
 
             var command = CommandParser.ParseArgsToCommand(args);
-
             var commandResult = await command.ExecuteAsync(cts.Token).ConfigureAwait(false);
-            if (commandResult.Outcome != CommandOutcome.Success)
-            {
-                Console.Error.WriteLine(commandResult.Result);
-            }
-            else
+            if (commandResult.Outcome == CommandOutcome.Success)
             {
                 Console.Out.WriteLine(commandResult.Result);
             }
-
+            else
+            {
+                Console.Error.WriteLine(commandResult.Result);
+                return 1;
+            }
             return 0;
         }
 
