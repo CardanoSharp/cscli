908700d9aed0389c1be4d5633f2eedc69b142462
safestak-keith <keith@safestak.com>
2022-03-25 16:57:49 +1100

    Added GH build and release workflows

--- build.ps1
+++ build.ps1
@@ -1,4 +1,11 @@
 dotnet restore
 dotnet build --no-restore
 dotnet test --no-build
-dotnet publish Src/ConsoleTool/CsCli.ConsoleTool.csproj -c Release -o release --nologo
+dotnet pack --no-build Src/ConsoleTool/CsCli.ConsoleTool.csproj -o nupkg -c Release
+
+# Generate cross-platform executables
+#dotnet publish Src/ConsoleTool/CsCli.ConsoleTool.csproj -c Release -o release --nologo
+#dotnet publish -r win-x64 Src/ConsoleTool/CsCli.ConsoleTool.csproj -c Release -o release -p:PublishSingleFile=true --self-contained true
+#dotnet publish -r linux-x64 Src/ConsoleTool/CsCli.ConsoleTool.csproj -c Release -o release -p:PublishSingleFile=true --self-contained true
+#dotnet publish -r osx-x64 Src/ConsoleTool/CsCli.ConsoleTool.csproj -c Release -o release -p:PublishSingleFile=true --self-contained true
+#dotnet publish -r osx.12-arm64 Src/ConsoleTool/CsCli.ConsoleTool.csproj -c Release -o release -p:PublishSingleFile=true --self-contained true
