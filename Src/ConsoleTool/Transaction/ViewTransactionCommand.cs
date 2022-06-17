17bcae58a70c10b7a60984cb8a4666fc5c288bd8
safestak-keith <keith@safestak.com>
2022-06-17 15:05:59 +1000

    Started implementing tx deserialisation to 'friendly' transaction object

--- Src/ConsoleTool/Transaction/ViewTransactionCommand.cs
+++ Src/ConsoleTool/Transaction/ViewTransactionCommand.cs
@@ -1,7 +1,30 @@
-﻿using CardanoSharp.Wallet.Extensions.Models.Transactions;
+﻿using CardanoSharp.Wallet.Extensions;
+using CardanoSharp.Wallet.Extensions.Models.Transactions;
+using CardanoSharp.Wallet.Models.Addresses;
+using CardanoSharp.Wallet.Models.Transactions;
 using System.Text.Json;
 
 namespace Cscli.ConsoleTool.Transaction;
+
+public record TxIn(
+    string TransactionId,
+    uint TransactionIndex);
+
+public record TxOut(
+    string Address,
+    AggregateValue Value);
+
+public record TransactionBody(
+    IEnumerable<TxIn> TransactionInputs,
+    IEnumerable<TxOut> TransactionOutputs,
+    IEnumerable<NativeAssetValue> NativeAssets,
+    ulong Fee,
+    uint? Ttl,
+    string MetadataHash,
+    uint? TransactionStartInterval);
+
+public record Transaction(
+    TransactionBody TransactionBody);
 
 public class ViewTransactionCommand : ICommand
 {
@@ -17,7 +40,19 @@ public class ViewTransactionCommand : ICommand
         }
 
         var tx = txCborBytes.DeserializeTransaction();
-        var json = JsonSerializer.Serialize(tx);
+
+        var transaction = new Transaction(
+            new TransactionBody(
+                tx.TransactionBody.TransactionInputs.Select(txI => new TxIn(txI.TransactionId.ToStringHex(), txI.TransactionIndex)).ToArray(),
+                tx.TransactionBody.TransactionOutputs.Select(
+                    txO => MapTxOut(txO)).ToArray(),
+                MapNativeAssets(tx.TransactionBody.Mint),
+                tx.TransactionBody.Fee,
+                tx.TransactionBody.Ttl,
+                tx.TransactionBody.MetadataHash,
+                tx.TransactionBody.TransactionStartInterval));
+
+        var json = JsonSerializer.Serialize(transaction);
 
         return ValueTask.FromResult(CommandResult.Success(json));
     }
@@ -47,5 +82,20 @@ public class ViewTransactionCommand : ICommand
             }
         }
         return (!validationErrors.Any(), Array.Empty<byte>(), validationErrors);
+    }
+
+    private static TxOut MapTxOut(TransactionOutput txO)
+    {
+        return new TxOut(
+            new Address("addr", txO.Address).ToString(), // TODO: address deserialization based on network
+            new AggregateValue(txO.Value.Coin, MapNativeAssets(txO.Value.MultiAsset).ToArray()));
+    }
+
+    private static NativeAssetValue[] MapNativeAssets(IDictionary<byte[], NativeAsset> multiAsset)
+    {
+        return multiAsset.Keys.SelectMany(
+            maKey => multiAsset[maKey].Token.Select(
+                mat => new NativeAssetValue(maKey.ToStringHex(), mat.Key.ToStringHex(), mat.Value)))
+            .ToArray();
     }
 }
